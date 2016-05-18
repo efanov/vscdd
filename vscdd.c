99f0ec7ded512d6358154eb4eb369ac791691cb4
AlexandraMerkel <gidrargirum.games@gmail.com>
2016-05-19 00:23:26 +0300

    Merkel branch (#2)
    * Update vscdd.c
    
    3 task
    
    * Update vscdd.c
    
    fix 3rd task + 1st task + 2nd task in progress
    
    * Update vscdd.c
    
    2nd task

--- vscdd.c
+++ vscdd.c
@@ -12,6 +12,8 @@
 #include <linux/types.h>        /* dev_t */
 #include <linux/cdev.h>
 #include <asm/uaccess.h>        /* copy_*_user */
+#include <linux/version.h> 
+#include <linux/device.h>
 
 #ifndef MODULE_NAME
 #define MODULE_NAME "vscdd"
@@ -38,6 +40,9 @@ static int device_open = 0;
  */ 
 struct cdev *cdev;
 
+static struct class *devclass;
+
+static int cur_size = 0;
 /* 
  * Память устройства
  */
@@ -48,6 +53,13 @@ static char *vscdd_buffer;
  */
 int vscdd_open(struct inode *inode, struct file *filp)
 {
+	if (device_open) {
+		pr_info("=== Device is already opened ===\n");
+		return -EBUSY;
+	}
+	device_open++;
+	зr_info("=== Opening device ===\n");
+	try_module_get(THIS_MODULE);
 	return 0;
 }
 
@@ -56,9 +68,36 @@ int vscdd_open(struct inode *inode, struct file *filp)
  */
 int vscdd_release(struct inode *inode, struct file *filp)
 {
+	device_open--;
+	module_put(THIS_MODULE);
 	return 0;
 }
 
+
+loff_t llseek(struct file *filp, loff_t off, int whence)
+{
+  loff_t new_pos;
+
+  switch(whence) {
+   case 0: 
+    new_pos = off;
+    break;
+
+   case 1: 
+    new_pos = filp->f_pos + off;
+    break;
+
+   case 2: 
+    new_pos = cur_size + off;
+    break;
+
+   default:
+    return -EINVAL;
+  }
+  if (new_pos<0) return -EINVAL;
+  filp->f_pos = new_pos;
+  return new_pos;
+}
 /*
  * Функция чтения из устройства
  */
@@ -118,7 +157,7 @@ ssize_t vscdd_write(struct file *filp, const char __user *buf, size_t count, lof
  */
 struct file_operations vscdd_fops = {
 	.owner =    THIS_MODULE,
-	//.llseek =   llseek,
+	.llseek =   llseek,
 	.read =     vscdd_read,
 	.write =    vscdd_write,
 	.open =     vscdd_open,
@@ -130,7 +169,15 @@ struct file_operations vscdd_fops = {
  */
 static void __exit vscdd_exit(void) 
 {
+	dev_t dev;
 	dev_t devno = MKDEV(major, minor);
+   	int i;
+   	for( i = 0; i < count; i++ ) {
+      		dev = MKDEV( major, minor + i );
+      		device_destroy( devclass, dev );
+   	}
+    	class_destroy( devclass );
+	
 	if (cdev) {
 		cdev_del(cdev);
 	}
@@ -150,6 +197,7 @@ static void __exit vscdd_exit(void)
 static int __init vscdd_init(void)
 {
 	int result;
+	int i;
 	dev_t dev = 0;
 	result = 0;
 
@@ -180,6 +228,20 @@ static int __init vscdd_init(void)
 		pr_info("=== vscdd: cdev_add error ===\n");
 	}
 	pr_info( "=== vscdd: %d:%d ===\n", major, minor);
+	
+	devclass = class_create( THIS_MODULE, "vscdd_class" ); /* struct class* */
+   	for( i = 0; i < count; i++ ) {
+#define DEVNAME "vscdd"
+      	dev = MKDEV( major, minor + i );
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,26)
+
+      device_create( devclass, NULL, dev, "%s_%d", DEVNAME, i );
+#else
+
+      device_create( devclass, NULL, dev, NULL, "%s_%d", DEVNAME, i );
+#endif
+   }
+   	pr_info( "======== module installed %d:[%d-%d] ===========\n", MAJOR( dev ), minor, MINOR( dev ) ); 
 
 	vscdd_buffer = kzalloc(100 * sizeof (*vscdd_buffer), GFP_KERNEL);
 	if (!vscdd_buffer) {
@@ -202,7 +264,7 @@ static int __init vscdd_init(void)
 module_init(vscdd_init);
 module_exit(vscdd_exit);
 
-MODULE_LICENSE("DUAL BSD/GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR ("МИФИ");
 MODULE_DESCRIPTION("Шаблон для разработки драйвера символьного устройства");
 MODULE_VERSION("20160503");
